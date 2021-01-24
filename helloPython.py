5d608c5d069c5d6b81713a003059284d05fc678a
DevOps Guy <devops@sprintqa.com>
2021-01-23 22:24:35 -0700

    deleted comments

--- helloPython.py
+++ helloPython.py
@@ -17,24 +17,3 @@ while i < 11:
     #     print('You lost your 10 chances')
     if i==10 and guess!=secret_num:
         print('You lost your 10 chances')
-
-
-# need random number generator between 1 and 100
-
-
-# need to keep track of number of times user is guessing 
-
-
-# while user guess is not equal to random guess and number of times user has is less than 10 continue
-
-
-# if user guess is higher than random guess then print “higher” and display remaining number of times
-
-
-# if user guess is lower than random guess then print “lower” and display remaining number of times 
-
-
-# else print “congrats, you guessed the random number”
-
-
-
